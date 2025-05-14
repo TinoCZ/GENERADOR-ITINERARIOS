a6a2e593f18f61325193c77c4e11246cc1a3790a
Valentino <89651139+TinoCZ@users.noreply.github.com>
2025-05-14 18:41:27 -0300

    Actualizar app.py con las modificaciones

--- app.py
+++ app.py
@@ -37,10 +37,7 @@ def generar_itinerario():
         return jsonify({'response': 'Hubo un error al generar el itinerario.'}), 500
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(debug=True)
 
 
 
